9f356b9b5873f06c4a4a33cb3a51a2847eb4fb1f
keval <kevalcholera@gmail.com>
2018-03-20 16:04:32 +0530

    kiosk integrate

--- app/src/main/java/com/example/keval/e_moss/KioskCenter.java
+++ app/src/main/java/com/example/keval/e_moss/KioskCenter.java
@@ -1,102 +1,110 @@
 package com.example.keval.e_moss;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
+import android.widget.ListView;
+
+import com.example.keval.e_moss.Utils.AdapterKioskCenter;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class KioskCenter extends AppCompatActivity {
+
+    ListView lvKioskCenter;
+    AdapterKioskCenter adapterKioskCenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kiosk_center);
 
-        TextView t1, t2, t3, t4, t5, t6, t7, t8, t9;
-        t1 = (TextView) findViewById(R.id.textView1);
-        t2 = (TextView) findViewById(R.id.textView2);
-        t3 = (TextView) findViewById(R.id.textView3);
-        t4 = (TextView) findViewById(R.id.textView4);
-        t5 = (TextView) findViewById(R.id.textView5);
-        t6 = (TextView) findViewById(R.id.textView6);
-        t7 = (TextView) findViewById(R.id.textView7);
-        t8 = (TextView) findViewById(R.id.textView8);
-        t9 = (TextView) findViewById(R.id.textView9);
+        JSONArray arrayKioskMain = new JSONArray();
 
+        JSONObject objectKioskElement = new JSONObject();
+        objectKioskElement.optString("userName", "Rajesh Bhai");
+        objectKioskElement.optString("userAdd", "9,458,jaipur");
+        objectKioskElement.optString("userNumber", "+91 1236547890");
+        objectKioskElement.optString("userEmail", "rajesh@gmail.com");
+        objectKioskElement.optString("userCategory", "N.A.");
+        objectKioskElement.optString("userLat", "26.812615");
+        objectKioskElement.optString("userLon", "75.542911");
 
-        t1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(KioskCenter.this, MapsActivity.class);
-                i.putExtra("value", 1);
-                startActivity(i);
-            }
-        });
-        t2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(KioskCenter.this, MapsActivity.class);
-                i.putExtra("value", 2);
-                startActivity(i);
-            }
-        });
-        t3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(KioskCenter.this, MapsActivity.class);
-                i.putExtra("value", 3);
-                startActivity(i);
-            }
-        });
-        t4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(KioskCenter.this, MapsActivity.class);
-                i.putExtra("value", 4);
-                startActivity(i);
-            }
-        });
-        t5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(KioskCenter.this, MapsActivity.class);
-                i.putExtra("value", 5);
-                startActivity(i);
-            }
-        });
-        t6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(KioskCenter.this, MapsActivity.class);
-                i.putExtra("value", 6);
-                startActivity(i);
-            }
-        });
-        t7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(KioskCenter.this, MapsActivity.class);
-                i.putExtra("value", 7);
-                startActivity(i);
-            }
-        });
-        t8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(KioskCenter.this, MapsActivity.class);
-                i.putExtra("value", 8);
-                startActivity(i);
-            }
-        });
-        t9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(KioskCenter.this, MapsActivity.class);
-                i.putExtra("value", 9);
-                startActivity(i);
-            }
-        });
+        JSONObject objectKioskElement1 = new JSONObject();
+        objectKioskElement.optString("userName", "Hari Bhai");
+        objectKioskElement.optString("userAdd", "hshu,czjk,jaipur");
+        objectKioskElement.optString("userNumber", "+91 7412589630");
+        objectKioskElement.optString("userEmail", "hari@gmail.com");
+        objectKioskElement.optString("userCategory", "N.A.");
+        objectKioskElement.optString("userLat", "26.850240");
+        objectKioskElement.optString("userLon", "75.635481");
+
+        JSONObject objectKioskElement2 = new JSONObject();
+        objectKioskElement.optString("userName", "hello Bhai");
+        objectKioskElement.optString("userAdd", "9bcbhbd,vfjhvhfb,jaipur");
+        objectKioskElement.optString("userNumber", "+91 4521479632");
+        objectKioskElement.optString("userEmail", "hello@gmail.com");
+        objectKioskElement.optString("userCategory", "N.A.");
+        objectKioskElement.optString("userLat", "26.807279");
+        objectKioskElement.optString("userLon", "75.806288");
+
+        JSONObject objectKioskElement3 = new JSONObject();
+        objectKioskElement.optString("userName", "KIOLO Bhai");
+        objectKioskElement.optString("userAdd", "hcdhcjz,zczdckjzc,jaipur");
+        objectKioskElement.optString("userNumber", "+91 1214587412");
+        objectKioskElement.optString("userEmail", "kiolo@gmail.com");
+        objectKioskElement.optString("userCategory", "N.A.");
+        objectKioskElement.optString("userLat", "26.886130");
+        objectKioskElement.optString("userLon", "75.746367");
+
+        JSONObject objectKioskElement4 = new JSONObject();
+        objectKioskElement.optString("userName", "kujiji Bhai");
+        objectKioskElement.optString("userAdd", "kvjxx,hdvdx,jaipur");
+        objectKioskElement.optString("userNumber", "+91 45214785");
+        objectKioskElement.optString("userEmail", "kevgjk@gmail.com");
+        objectKioskElement.optString("userCategory", "N.A.");
+        objectKioskElement.optString("userLat", "26.886519");
+        objectKioskElement.optString("userLon", "75.834123");
+
+        JSONObject objectKioskElement5 = new JSONObject();
+        objectKioskElement.optString("userName", "LOPOLO Bhai");
+        objectKioskElement.optString("userAdd", "cjjhkdj,dhkdhk,jaipur");
+        objectKioskElement.optString("userNumber", "+91 4125896325");
+        objectKioskElement.optString("userEmail", "hfhfshdfk@gmail.com");
+        objectKioskElement.optString("userCategory", "N.A.");
+        objectKioskElement.optString("userLat", "26.870782");
+        objectKioskElement.optString("userLon", "75.692110");
+
+        JSONObject objectKioskElement6 = new JSONObject();
+        objectKioskElement.optString("userName", "dhsjfhkf Bhai");
+        objectKioskElement.optString("userAdd", "fsfhfkjs,fdsfjkh,jaipur");
+        objectKioskElement.optString("userNumber", "+91 4521478965");
+        objectKioskElement.optString("userEmail", "oijuhyu@gmail.com");
+        objectKioskElement.optString("userCategory", "N.A.");
+        objectKioskElement.optString("userLat", "26.895705");
+        objectKioskElement.optString("userLon", "75.783491");
+
+        JSONObject objectKioskElement7 = new JSONObject();
+        objectKioskElement.optString("userName", "Ramesh Bhai");
+        objectKioskElement.optString("userAdd", "hvjhkjd,fjdkhfdx,jaipur");
+        objectKioskElement.optString("userNumber", "+91 5565656556");
+        objectKioskElement.optString("userEmail", "ramesh@gmail.com");
+        objectKioskElement.optString("userCategory", "N.A.");
+        objectKioskElement.optString("userLat", "26.822683");
+        objectKioskElement.optString("userLon", "75.543727");
+
+        arrayKioskMain.put(objectKioskElement);
+        arrayKioskMain.put(objectKioskElement1);
+        arrayKioskMain.put(objectKioskElement2);
+        arrayKioskMain.put(objectKioskElement3);
+        arrayKioskMain.put(objectKioskElement4);
+        arrayKioskMain.put(objectKioskElement5);
+        arrayKioskMain.put(objectKioskElement6);
+        arrayKioskMain.put(objectKioskElement7);
+
+        lvKioskCenter = (ListView) findViewById(R.id.lvKioskCenter);
+        adapterKioskCenter = new AdapterKioskCenter(this, arrayKioskMain);
+        lvKioskCenter.setAdapter(adapterKioskCenter);
 
     }
 }
--- app/src/main/java/com/example/keval/e_moss/MapsActivity.java
+++ app/src/main/java/com/example/keval/e_moss/MapsActivity.java
@@ -10,60 +10,40 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    JSONObject mainObject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        mainObject = new JSONObject();
+
+        String data = getIntent().getStringExtra("object");
+        try {
+            mainObject = new JSONObject(data);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(26.916418, 75.777640);
-//
-//        ArrayList<Double> latArray = new ArrayList();
-//        latArray.add(26.916418);
-//        latArray.add(26.925810);
-//        latArray.add(26.836092);
-//        latArray.add(26.928137);
-//        latArray.add(26.888284);
-//        latArray.add(26.922057);
-//        latArray.add(26.941132);
-//        latArray.add(26.881025);
-//        latArray.add(26.874933);
-//
-//        ArrayList<Double> lonArray = new ArrayList();
-//        lonArray.add(75.778338);
-//        lonArray.add(75.792735);
-//        lonArray.add(75.794057);
-//        lonArray.add(75.657550);
-//        lonArray.add(75.795695);
-//        lonArray.add(75.683619);
-//        lonArray.add(75.743351);
-//        lonArray.add(75.767172);
-//        lonArray.add(75.764415);
-//
-//        for (int i = 0; i < latArray.size() - 1; i++) {
-//
-//            sydney = new LatLng(latArray.get(i), lonArray.get(i));
-//
-//            mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Jaipur"));
-//            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-//        }
+        LatLng sydney = new LatLng(mainObject.optInt("userLat"), mainObject.optInt("userLon"));
 
-        LatLng sydney = new LatLng(26.877451, 75.811652);
-
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Jaipur"));
+        mMap.addMarker(new MarkerOptions().position(sydney).title(mainObject.optString("userAdd")));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
     }
